6fb62036e37bbdd1737ae027078636373b3f5833
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-11-25 08:00:00 +0200

    Add CORS Headers settings

--- mastermindz_backend/mastermindz_backend/settings.py
+++ mastermindz_backend/mastermindz_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'mastermindz',
 ]
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mastermindz_backend.urls'
