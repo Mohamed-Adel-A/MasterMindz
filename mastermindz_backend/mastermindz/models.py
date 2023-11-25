8475f599f641c1807b3687de72e9203624c6e323
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-11-25 07:32:17 +0200

    Add Models

--- mastermindz_backend/mastermindz/models.py
+++ mastermindz_backend/mastermindz/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='groups_led')
+    category = models.CharField(max_length=255)
+    tags = models.ManyToManyField('GroupTags')
+    members = models.ManyToManyField(User, through='GroupMembers', related_name='groups_joined')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class GroupMembers(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+
+class GroupChat(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class PrivateChat(models.Model):
+    participants = models.ManyToManyField(User, related_name='private_chats')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE) # related_name='sent_private_chats')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE) # related_name='received_private_chats')
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class GroupTags(models.Model):
+    name = models.CharField(max_length=255)
