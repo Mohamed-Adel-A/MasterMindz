8b34d0c53005bc8726c001af918e2280d0658c8f
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-11-26 09:51:49 +0200

    Add views and urls

---
+++ mastermindz_backend/mastermindz/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from .views import (
+    UserListView, UserDetailView, GroupListView, GroupDetailView,
+    GroupTagListView, GroupTagDetailView, GroupChatListView, PrivateChatListView,
+    GroupMembersView,
+)
+
+urlpatterns = [
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('groups/', GroupListView.as_view(), name='group-list'),
+    path('groups/<int:pk>/', GroupDetailView.as_view(), name='group-detail'),
+    path('group-tags/', GroupTagListView.as_view(), name='group-tag-list'),
+    path('group-tags/<int:pk>/', GroupTagDetailView.as_view(), name='group-tag-detail'),
+    path('group-chats/', GroupChatListView.as_view(), name='group-chat-list'),
+    path('private-chats/', PrivateChatListView.as_view(), name='private-chat-list'),
+    path('groups/<int:group_id>/members/', GroupMembersView.as_view(), name='group-members'),
+]
+
+urlpatterns = [
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('groups/', GroupListView.as_view(), name='group-list'),
+    path('groups/<int:pk>/', GroupDetailView.as_view(), name='group-detail'),
+    path('group-tags/', GroupTagListView.as_view(), name='group-tag-list'),
+    path('group-tags/<int:pk>/', GroupTagDetailView.as_view(), name='group-tag-detail'),
+    path('meetings/', MeetingListView.as_view(), name='meeting-list'),
+    path('meetings/<int:pk>/', MeetingDetailView.as_view(), name='meeting-detail'),
+    path('group-chats/', GroupChatListView.as_view(), name='group-chat-list'),
+    path('private-chats/', PrivateChatListView.as_view(), name='private-chat-list'),
+]
--- mastermindz_backend/mastermindz/views.py
+++ mastermindz_backend/mastermindz/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .models import Group, GroupMember, GroupChat, PrivateChat, GroupTag
+from .serializers import UserSerializer, GroupSerializer, GroupMemberSerializer
+from .serializers import GroupTagSerializer, GroupChatSerializer, PrivateChatSerializer
 
-# Create your views here.
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GroupListView(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class GroupDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class GroupMembersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        group_id = self.kwargs['group_id']
+        return User.objects.filter(groups_joined__id=group_id)
+
+class GroupTagListView(generics.ListCreateAPIView):
+    queryset = GroupTag.objects.all()
+    serializer_class = GroupTagSerializer
+
+class GroupTagDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GroupTag.objects.all()
+    serializer_class = GroupTagSerializer
+
+class GroupChatListView(generics.ListCreateAPIView):
+    queryset = GroupChat.objects.all()
+    serializer_class = GroupChatSerializer
+
+class PrivateChatListView(generics.ListCreateAPIView):
+    queryset = PrivateChat.objects.all()
+    serializer_class = PrivateChatSerializer
